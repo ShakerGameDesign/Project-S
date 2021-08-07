b8ea4840fba2cd21ef3eaaa770eca6048169d4a6
Kanedsodas <kanedsodas@gmail.com>
2021-08-06 21:01:33 -0400

    Added Mountain Spawner

--- Assets/Scripts/WorldInfo.cs
+++ Assets/Scripts/WorldInfo.cs
@@ -2,19 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WorldInfo
+public class WorldInfo : MonoBehaviour
 {
-    public static WorldInfo i = new WorldInfo();
+    public static WorldInfo i;
     public static Player player;
-    private WorldInfo() { }
+
+    public GameObject worldLocAnchorGO;
+
+    private void Start()
+    {
+        i = this;
+
+    }
 
     public float worldLocation { get; private set; }
+    public float PathCenAtWorldLoc { get; private set; }
     public static int camSideViewDistance = 35;
 
     public static void setWorldLoc(float nwl)
     {
         i.worldLocation = nwl;
-        player.cameraAnchor.transform.position = new Vector3(i.worldLocation, 0, PathGenerator.pathGen.GetPathCenterAtPos(camSideViewDistance));
+        i.PathCenAtWorldLoc = PathGenerator.pathGen.GetPathCenterAtPos(camSideViewDistance);
+        i.worldLocAnchorGO.transform.position = i.worldLocAnchorGO.transform.position.SX(nwl);
+        i.worldLocAnchorGO.transform.position = i.worldLocAnchorGO.transform.position.SZ(i.PathCenAtWorldLoc);
+
+        player.cameraAnchor.transform.position = new Vector3(i.worldLocation, 0, i.PathCenAtWorldLoc);
         PathGenerator.pathGen.transform.position = PathGenerator.pathGen.transform.position.SX(i.worldLocation - camSideViewDistance);
     }
 }
